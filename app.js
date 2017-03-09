cdfe18d8d9eb4d77fba375e0a59642b3c41aee33
M-Abozaid <aapozaid@gmail.com>
2017-03-09 23:50:27 +0200

    Adding bootstrap files using bower and adding bower_components folder as an express.static in app.js

--- app.js
+++ app.js
@@ -64,6 +64,7 @@ app.use(expressValidator({
 
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'bower_components')));
 
 // flash & // Express messages
 app.use(flash());
